786cfd802d62ce64ca874873cd59953ded67148c
jdpore <pore.johndexter@ue.edu.ph>
2021-03-31 03:40:45 +0800

    Project Folder with Front Window

---
+++ InBetween/app/src/main/java/com/example/in_between/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.in_between;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
